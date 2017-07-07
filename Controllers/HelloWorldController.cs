9e89811a14ca341e7c1da12a0a6cbcc58f28487c
Huey Lee <leejinhuey@gmail.com>
2017-07-07 15:06:51 +0800

    using Razor view templates now. dotnet watch is a lifesaver

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -4,13 +4,15 @@ using System.Text.Encodings.Web;
 namespace MvcMovie.Controllers{
   public class HelloWorldController : Controller {
     // GET /HelloWorld/
-    public string Index(){
-      return "Default action";
+    public IActionResult Index(){
+      return View();
     }
 
     // GET /HelloWorld/Welcome/
-    public string Welcome(string name, int ID = 1){
-      return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+    public IActionResult Welcome(string name, int numTimes = 1){
+      ViewData["Message"] = $"Hello {name}";
+      ViewData["NumTimes"] = numTimes;
+      return View();
     }
   }
 }
